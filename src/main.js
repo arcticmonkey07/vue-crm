3e5279d18c7ef48654bf9150a36346560e98a4e7
arcticmonkey07 <arcticmonkey07@gmail.com>
2019-11-14 20:54:25 +0300

    planning page + tooltip

--- src/main.js
+++ src/main.js
@@ -6,6 +6,7 @@ import router from './router'
 import store from './store'
 import dateFilter from '@/filters/date.filter'
 import currencyFilter from '@/filters/currency.filter'
+import tooltipDirective from '@/directives/tooltip.directive'
 import messagePlugin from '@/utils/message.plugin'
 import Loader from '@/components/app/Loader'
 import 'materialize-css/dist/js/materialize.min'
@@ -20,6 +21,7 @@ Vue.use(messagePlugin)
 Vue.use(Vuelidate)
 Vue.filter('date', dateFilter)
 Vue.filter('currency', currencyFilter)
+Vue.directive('tooltip', tooltipDirective)
 Vue.component('Loader', Loader)
 
 firebase.initializeApp({
